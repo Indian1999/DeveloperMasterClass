b1822059cf812563f820e07f0467891769c58b1c
Indian1999 <haszab99@gmail.com>
2025-02-23 12:30:41 +0100

    create table

--- sql_introduction/main.py
+++ sql_introduction/main.py
@@ -18,6 +18,16 @@ def show_databases():
 def print_results():
     for result in cursor:
         print(result)
+        
+def create_table(table_name:str, field_names:list, field_types:list) -> None:
+    # CREATE TABLE table_name (name VARCHAR(255), address VARCHAR(255))
+    fields = ""
+    for i in range(len(field_names)):
+        fields += field_names[i] + " " + field_types[i] + ", "
+    fields = fields[:-2]
+    cursor.execute(f"CREATE TABLE {table_name} ({fields})")
     
-#create_database("logiscool")
-#show_databases()
+table_name = "customers"
+field_names = ["name", "address", "country", "postal_code", "age", "email", "telephone"]
+field_types = ["VARCHAR(255)","VARCHAR(255)","VARCHAR(255)", "INT(255)", "INT(255)","VARCHAR(255)","VARCHAR(255)"]
+create_table(table_name, field_names, field_types)
