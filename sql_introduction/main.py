5db9482230141e4ee775c237bd10126c6947b366
Indian1999 <haszab99@gmail.com>
2025-03-02 18:46:15 +0100

    upload_data_to()

--- sql_introduction/main.py
+++ sql_introduction/main.py
@@ -1,4 +1,5 @@
 import mysql.connector
+import pandas as pd # pip install pandas (terminálba)
 
 db = mysql.connector.connect(
     host = "localhost",
@@ -36,8 +37,20 @@ def clear_table(table_name):
     cursor.execute(f"TRUNCATE TABLE {table_name}")
     cursor.execute(f"ALTER TABLE {table_name} AUTO_INCREMENT = 1;")
 
+def upload_data_to(data, table_name):
+    for row in data.iterrows():
+        cursor.execute(f"INSERT INTO {table_name} (name, address, country, postal_code, age, email, telephone) VALUES ('{row[1]['name']}', '{row[1]['address']}', '{row[1]['country']}', '{row[1]['postal_code']}', '{row[1]['age']}', '{row[1]['email']}', '{row[1]['telephone']}')")
+    
 table_name = "customers"
 field_names = ["name", "address", "country", "postal_code", "age", "email", "telephone"]
 field_types = ["VARCHAR(255)","VARCHAR(255)","VARCHAR(255)", "INT(255)", "INT(255)","VARCHAR(255)","VARCHAR(255)"]
 
 clear_table("customers")
+
+data = pd.read_csv("sql_introduction\generated_data.csv", delimiter = ";")
+
+upload_data_to(data, "customers")
+cursor.execute("SELECT * FROM customers")
+print_results()
+
+
