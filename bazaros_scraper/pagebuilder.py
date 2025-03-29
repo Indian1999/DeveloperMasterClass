4bc0c03483aec182269b712d9dff86dfc238456f
Indian1999 <haszab99@gmail.com>
2025-03-29 15:32:54 +0100

    update

--- bazaros_scraper/pagebuilder.py
+++ bazaros_scraper/pagebuilder.py
@@ -3,13 +3,14 @@ def generate_div_string_from_dict(dict:dict) -> str:
     output += f"\t\t<h1>{dict['title']}</h1>\n"
     output += f"\t\t<img src = {dict['img-url']} alt='image'>\n"
     output += f"\t\t<p>Price: {dict['price']} {dict['currency']}</p>\n"
-    output = "\t</div>\n"
+    output += "\t</div>\n"
     return output
 
 def generate_html_from_template(template_path, destination_path, div_list):
     template = ""
     with open(template_path, "r", encoding="utf-8") as f:
-        template = f.readlines()
+        template = f.readlines()# ["", "",""]
+        template = "\n".join(template)
     for div in div_list:
         template += div
     template += "</body>"
