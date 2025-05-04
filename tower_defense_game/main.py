c31cc144ff8553f5048988ae1439ac5ebce1eb4b
Indian1999 <haszab99@gmail.com>
2025-05-04 14:07:23 +0200

    Path created

--- tower_defense_game/main.py
+++ tower_defense_game/main.py
@@ -1,9 +1,26 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.graphics import Line, Color, Rectangle
+from kivy.core.window import Window
 
 class GameWidget(Widget):
     def __init__(self, **kwarg):
         super().__init__()
+        self.path_points = [
+            (0, Window.height // 5), 
+            (Window.width // 4, Window.height // 5),
+            (Window.width // 4, Window.height // 5 * 4),
+            (Window.width // 4 * 3, Window.height // 5 * 4),
+            (Window.width // 4 * 3, 0)
+        ]
+        self.draw_path()
+        
+    def draw_path(self):
+        with self.canvas:
+            Color(0.6, 0.4, 0.2) # Barna
+            Line(points=sum(self.path_points, ()), width=40, cap = "round")
+        
+    
 
 
 class TowerDefenseApp(App):
